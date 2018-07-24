0417ac404e54590e0f0b350b81b4ead125e522a8
Jenn Wheeler <jwheeler@antiochcollege.edu>
2018-07-24 09:10:05 -0400

    just pushing some more work done on this

--- src/memmap/unix.cr
+++ src/memmap/unix.cr
@@ -2,13 +2,14 @@ require "./mman"
 
 SC_PAGESIZE = 30
 
-module Memmap::Mapped
+module Memmap
   extend self
 
-  enum Flags
+  enum Flag
     Shared  = LibC::MAP_SHARED
     Private = LibC::MAP_PRIVATE
     Fixed   = LibC::MAP_FIXED
+    Anon    = LibC::MAP_ANONYMOUS
   end
 
   enum Prot
@@ -18,35 +19,92 @@ module Memmap::Mapped
     None    = LibC::PROT_NONE
   end
 
-  # A memory map backed by a specified file.
+  # A memory mapped buffer backed by a specified file.
   class MappedFile
-    PAGE_SIZE = LibC.sysconf(SC_PAGESIZE)
+    PAGE_SIZE = LibC.sysconf(SC_PAGESIZE).as(LibC::SizeT)
+    @map : UInt8*
+    @len : LibC::SizeT
 
-    def initialize(@filename : String, @flags : Flags, @prot : Prot)
+    def initialize(@filepath : String, mode = "r" @flags : Flag, @prot : Prot, offset : LibC::SizeT = 0)
+      @len = File.info(@filepath).size
+      alignment = offset % PAGE_SIZE
+      aligned_offset = offset - alignment
+      aligned_len = len + alignment
+
+      (@flags, @prot) = parse_mode(mode)
+
+      @map = allocate(aligned_len, aligned_offset)
     end
 
-    def allocate
+    def finalize
+      # TODO: calculate len stuff
+      len = 0.as(LibC::SizeT)
+      if LibC.munmap(@map, len) == -1
+        raise Errno.new("Error unmapping file")
+      end
+    end
+
+    protected def allocate(aligned_len : SizeT, aligned_offset : SizeT) : UInt8*
       fd =
-        if @flags == Flags::Shared && File.writable?(@filename)
-          File.open(@filename, mode="rw").fd
+        if @flags == Flag::Shared && File.writable?(@filepath)
+          File.open(@filepath, mode="rw").fd
         elsif
-          File.open(@filename, mode="r").fd
+          File.open(@filepath, mode="r").fd
         else
-          raise Errno.new("Unable to open file '#{@filename}'")
+          raise Errno.new("Unable to open file '#{@filepath}'")
         end
+
+      ptr = LibC.mmap(nil, aligned_len, prot, flags, fd, aligned_offset)
+
+      if ptr == LibC::MAP_FAILED
+        raise Errno.new("Unable to create map")
+      end
+
+      # Cast the `void*` returned by `mmap()` to a `char*`, `UInt8*` in Crystal
+      Pointer(UInt8).new(ptr.address)
     end
 
+    protected def parse_mode(mode : String)
+      if mode.size == 0
+        raise "Invalid access mode"
+      end
+
+      prot = Array(Prot).new
+
+      case mode[0]
+      when 'r'
+        prot.push(Prot::Read)
+      when 'w'
+        prot.push(Prot::Write)
+      end
+
+      case mode[1]
+      when 'w'
+        prot.push(Prot::Read)
+      when 'a'
+        flags = Flag::Anon
+      end
+    end
+
+    # Flush changes made in the map back into the filesystem.
+    # Synchronous/blocking version.
     def flush
+      LibC.msync(@map, @len, LibC::MS_SYNC)
     end
 
     def flush_async
     end
 
     def make_read_only
+      # TODO: calculate len stuff
+      len = 0.as(LibC::SizeT)
+      LibC.mprotect(@map, len, Prot::Read)
     end
-  end
 
-  class MappedOptions
-
+    def make_writable
+      # TODO: calculate len stuff
+      len = 0.as(LibC::SizeT)
+      LibC.mprotect(@map, len, Prot::Write | Prot::Read)
+    end
   end
 end
