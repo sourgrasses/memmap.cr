da86819bd641530a060cd076a950052d91d5cab2
Jenn Wheeler <jawheeler@wm.edu>
2021-02-15 12:32:59 -0500

    mremap consts should only exist on linux

--- src/memmap/mman.cr
+++ src/memmap/mman.cr
@@ -2,14 +2,12 @@ lib LibC
   MS_ASYNC      = 1
   MS_SYNC       = 4
   MS_INVALIDATE = 2
-  # SC_PAGESIZE   = 30
-
-  MREMAP_MAYMOVE    = 1
-  MREMAP_FIXED      = 2
 
   fun msync(addr : Void*, len : SizeT, flags : Int)
 
   {% if flag?(:linux) %}
+    MREMAP_MAYMOVE    = 1
+    MREMAP_FIXED      = 2
     fun mremap(old_addr : Void*, old_size : SizeT, new_size : SizeT, flags : Int) : Void*
   {% end %}
 end
